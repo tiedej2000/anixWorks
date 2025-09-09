563f6956cb7dc27fe245338d703519e0a30647ce
tiedej2000 <tiedejustin@protonmail.de>
2025-09-09 13:59:54 +0200

    work 0.2 hovers

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+/* pill modals */
+
 const pillModuls = document.querySelectorAll('.pill');
 
 pillModuls.forEach(pill => {
@@ -11,3 +13,57 @@ pillModuls.forEach(pill => {
     modal.classList.remove('show')
   });
 });
+
+/* custom cursor */
+
+const customCursor = document.querySelector('.cursor')
+
+const moveCursor = (e) =>{
+    const mouseY = e.clientY
+    const mouseX = e.clientX
+
+    customCursor.style.left = `${mouseX}px`
+    customCursor.style.top = `${mouseY}px`
+}
+
+window.addEventListener('mousemove', moveCursor)
+
+/* cursor events */
+
+const hoverElement = document.querySelectorAll('a, .pill, .accesibility') 
+
+hoverElement.forEach(el =>{
+    el.addEventListener('mouseenter', () =>{
+        customCursor.classList.add('el-hover')
+    })
+
+    el.addEventListener('mouseleave', () =>{
+        customCursor.classList.remove('el-hover')
+    })
+})
+
+const workImages = document.querySelectorAll('.work__img')
+
+workImages.forEach(img =>{
+    img.addEventListener('mouseenter', () =>{
+        customCursor.classList.add('img-hover')
+    })
+
+    img.addEventListener('mouseleave', () =>{
+        customCursor.classList.remove('img-hover')
+    })
+})
+
+/* accesibilty show */
+
+const accesibilityIcon = document.querySelector('.accesibility-icon')
+const accesibilityModal = document.querySelector('.accesibility-modal')
+
+accesibilityIcon.addEventListener('click', () =>{
+    console.log('clicked')
+    accesibilityModal.classList.toggle('show')
+})
+
+function closeModal() {
+  document.querySelector('.accesibility-modal').classList.remove('show');
+}
